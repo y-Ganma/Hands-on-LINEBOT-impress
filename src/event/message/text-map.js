315467abda0a9fff83b6832d92c7d3c592cfb47c
Yukinari <s1320075@u-aizu.ac.jp>
2024-07-23 17:52:50 +0900

    submit db aws

--- src/event/message/text-map.js
+++ src/event/message/text-map.js
@@ -1,4 +1,8 @@
+import {
+  createData, deleteData, readData, updateData,
+} from '../../crud.js';
 import { get } from '../../request.js';
+import { error } from '../../log.js';
 
 // ユーザーのプロフィールを取得する関数
 const getUserProfile = (event, client) => client.getProfile(event.source.userId);
@@ -455,4 +459,157 @@ export const messageMap = {
     type: 'text',
     text: `ここは${event.source.type}だよ！`,
   }),
+  Create: async (event, appContext) => {
+    const date = new Date();
+    await createData(event.source.userId, 'testData', `Data created at ${date}`, appContext);
+    return {
+      type: 'text',
+      text: 'データが作成されました',
+    };
+  },
+  Read: async (event, appContext) => {
+    const dbData = await readData(event.source.userId, 'testData', appContext);
+    return {
+      type: 'text',
+      text: `DBには以下のデータが保存されています\n\n${dbData.Items[0].Data}`,
+    };
+  },
+  Update: async (event, appContext) => {
+    const date = new Date();
+    await updateData(event.source.userId, 'testData', `Data created at ${date}`, appContext);
+    return {
+      type: 'text',
+      text: 'データを更新しました',
+    };
+  },
+  Delete: async (event, appContext) => {
+    await deleteData(event.source.userId, 'testData', appContext);
+    return {
+      type: 'text',
+      text: 'データを削除しました',
+    };
+  },
+  メモ: async (event, appContext) => {
+    const memoData = await readData(event.source.userId, 'memo', appContext);
+    if (memoData.Items[0]) {
+      return {
+        type: 'text',
+        text: `メモには以下のメッセージが保存されています\n\n${memoData.Items[0].Data}`,
+      };
+    }
+
+    return {
+      type: 'text',
+      text: 'メモが存在しません',
+    };
+  },
+  メモ開始: async (event, appContext) => {
+    await createData(event.source.userId, 'context', 'memoMode', appContext);
+    return {
+      type: 'text',
+      text: 'メモモードを開始しました',
+    };
+  },
+  ニュース: async () => {
+    // ニュースAPIのレスポンスを格納する変数を宣言
+    let newsApiRes;
+    // エラーハンドリング
+    try {
+      // APIのレスポンスをnewsApiResに格納
+      newsApiRes = (await get(`https://newsapi.org/v2/top-headlines?country=jp&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`)).data;
+    } catch (e) {
+      error(`news API error: ${e}`);
+      return {
+        type: 'text',
+        text: 'ニュースAPIのリクエストでエラーが発生しました',
+      };
+    }
+    // 返信するメッセージを作成
+    const message = {
+      type: 'flex',
+      altText: 'ニュース一覧',
+      contents: {
+        type: 'carousel',
+        // contentsはいったん空にしておく
+        contents: [],
+      },
+    };
+
+    // newsApiRes.articlesを取り出す(分割代入という記法)
+    const { articles } = newsApiRes;
+    // articles(配列)の長さ分ループを回す
+    // 配列の要素がarticleに格納される
+    articles.forEach((article) => {
+      // Carouselのcontentを作成
+      const content = {
+        type: 'bubble',
+        size: 'kilo',
+        hero: {
+          type: 'image',
+          // nullだったときはダミーのデータを挿入する(三項演算子という記法)
+          url: article.urlToImage === null ? 'https://raw.githubusercontent.com/shinbunbun/aizuhack-bot/master/media/imagemap.png' : article.urlToImage,
+          size: 'full',
+          aspectMode: 'cover',
+        },
+        body: {
+          type: 'box',
+          layout: 'vertical',
+          contents: [
+            {
+              type: 'text',
+              weight: 'bold',
+              size: 'sm',
+              wrap: true,
+              // nullだったときはダミーのデータを挿入する
+              text: article.title === null ? 'No title' : article.title,
+            },
+            {
+              type: 'text',
+              // nullだったときはダミーのデータを挿入する
+              text: article.publishedAt === null ? 'No publishedAt' : article.publishedAt,
+              size: 'xs',
+              color: '#a9a9a9',
+            },
+            {
+              type: 'text',
+              // nullだったときはダミーのデータを挿入する
+              text: article.description === null ? 'No description' : article.description.substring(0, 100),
+              size: 'sm',
+              wrap: true,
+            },
+          ],
+          spacing: 'md',
+        },
+        footer: {
+          type: 'box',
+          layout: 'vertical',
+          contents: [
+            {
+              type: 'button',
+              action: {
+                type: 'uri',
+                // nullだったときはダミーのデータを挿入する
+                uri: article.url === null ? 'https://example.com' : article.url,
+                // nullだったときはダミーのデータを挿入する
+                label: article.source.name === null ? 'No article source name' : article.source.name,
+              },
+              style: 'primary',
+            },
+          ],
+        },
+      };
+      // contents(配列)にcontentを追加
+      message.contents.contents.push(content);
+    });
+    return message;
+  },
+  蔵書検索: async (event, appContext) => {
+    // DBにコンテキストを追加
+    await createData(event.source.userId, 'context', 'bookSearchMode2', appContext);
+
+    return {
+      type: 'text',
+      text: '検索したい本のISBNを送ってください\n※本のISBNは「検索したい本のタイトル ISBN」などとGoogleで検索すれば取得できます',
+    };
+  },
 };
